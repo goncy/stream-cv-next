3fcbfe6b8ea80944df0a5aad767192a6876b0777
goncy <gonzalo.pozzo4@gmail.com>
2020-06-25 19:29:58 -0300

    Armamos nuestro cv bonito

--- components/post-preview.js
+++ components/post-preview.js
@@ -25,7 +25,6 @@ export default function PostPreview({
         <DateFormater dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
@@ -8,8 +9,7 @@ import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const [heroPost, ...morePosts] = allPosts;
   return (
     <>
       <Layout>
