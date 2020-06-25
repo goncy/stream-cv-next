7cbfcd2c792bc0de416782f166f4999f0aacde34
goncy <gonzalo.pozzo4@gmail.com>
2020-06-25 19:35:14 -0300

    Cambiamos el titulo

--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        CV de Gonzalo Pozzo.
+        CV de Goncy.
       </h1>
     </section>
   )
